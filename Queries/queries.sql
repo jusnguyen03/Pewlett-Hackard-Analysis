ddc8c21fa73a4febc6d6b41369200b2353583add
Justin Nguyen <tan.nguyen03@gmail.com>
2020-01-09 16:41:28 -0800

    Module 7.3.6 Complete

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -197,3 +197,27 @@ INNER JOIN dept_emp AS de
 ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
+
+SELECT  ri.emp_no,
+        ri.first_name,
+        ri.last_name,
+        d.dept_name
+INTO sales_info
+FROM retirement_info AS ri
+    INNER JOIN dept_emp AS de
+        ON (ri.emp_no = de.emp_no)
+    INNER JOIN departments AS d
+        ON (de.dept_no = d.dept_no)
+WHERE dept_name = 'Sales';
+
+SELECT  ri.emp_no,
+        ri.first_name,
+        ri.last_name,
+        d.dept_name
+INTO sales_develpment_info
+FROM retirement_info AS ri
+    INNER JOIN dept_emp AS de
+        ON (ri.emp_no = de.emp_no)
+    INNER JOIN departments AS d
+        ON (de.dept_no = d.dept_no)
+WHERE dept_name IN ('Sales', 'Development');
